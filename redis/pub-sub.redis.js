4b4795b3e468d34844d8247db09001c67246d62d
itz-devilx <intekhab118211989@gmail.com>
2026-05-01 01:30:58 +0530

    fix: add redis folder and make code modular

---
+++ redis/pub-sub.redis.js
@@ -0,0 +1,56 @@
+import { publisher, subscriber, redis } from './redis-connection.js';
+
+
+async function initRedisPubSub(io){
+  //we can also store in redis
+  const stateArray = [];
+  const ACTIVE_USER_KEY = "internal-server:active:user";
+
+
+  await subscriber.subscribe("internal-server:checkbox:click", "internal-server:active:user");
+  subscriber.on("message", async(channel, message)=>{
+    if(channel === "internal-server:checkbox:click"){
+      const {id, isChecked} = JSON.parse(message)
+
+      if(isChecked){
+        stateArray.push(id);
+      }
+      else{
+        const index = stateArray.indexOf(id);
+        stateArray.splice(index, 1);
+      }
+      io.emit('server:update', JSON.parse(message));
+    }
+
+    if(channel === ACTIVE_USER_KEY){
+      const userCount = await redis.get(ACTIVE_USER_KEY);
+      io.emit('server:active', userCount);
+    }
+  } )
+
+  io.on("connection", async (socket)=>{
+    console.log("Connection opened", socket.id);
+    const activeUserCount = await redis.get(ACTIVE_USER_KEY);
+    if(!activeUserCount){
+      await redis.set(ACTIVE_USER_KEY, 0);
+    }
+    let count = await redis.incr(ACTIVE_USER_KEY);
+    await publisher.publish(ACTIVE_USER_KEY, count);
+
+
+    socket.on('user:click', async (data)=>{
+      // console.log(data);
+      await publisher.publish("internal-server:checkbox:click", JSON.stringify(data));
+    })
+
+
+    socket.on('disconnect', async()=>{
+      count = await redis.decr(ACTIVE_USER_KEY);
+      await publisher.publish(ACTIVE_USER_KEY, count);
+    })
+
+  })
+}
+
+
+export default initRedisPubSub;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import http from 'http';
-import { publisher, subscriber, redis } from './redis-connection.js';
+import initRedisPubSub from './redis/pub-sub.redis.js';
 
 const PORT = process.env.PORT ?? 8000;
 
@@ -24,55 +24,7 @@ const PORT = process.env.PORT ?? 8000;
   // io.attach(server);
 
 
-  //we can also store in redis
-  const stateArray = [];
-  const ACTIVE_USER_KEY = "internal-server:active:user";
-
-
-  await subscriber.subscribe("internal-server:checkbox:click", "internal-server:active:user");
-  subscriber.on("message", async(channel, message)=>{
-    if(channel === "internal-server:checkbox:click"){
-      const {id, isChecked} = JSON.parse(message)
-
-      if(isChecked){
-        stateArray.push(id);
-      }
-      else{
-        const index = stateArray.indexOf(id);
-        stateArray.splice(index, 1);
-      }
-      io.emit('server:update', JSON.parse(message));
-    }
-
-    if(channel === ACTIVE_USER_KEY){
-      const userCount = await redis.get(ACTIVE_USER_KEY);
-      io.emit('server:active', userCount);
-    }
-  } )
-
-  io.on("connection", async (socket)=>{
-    console.log("Connection opened", socket.id);
-    const activeUserCount = await redis.get(ACTIVE_USER_KEY);
-    if(!activeUserCount){
-      await redis.set(ACTIVE_USER_KEY, 0);
-    }
-    let count = await redis.incr(ACTIVE_USER_KEY);
-    await publisher.publish(ACTIVE_USER_KEY, count);
-
-
-    socket.on('user:click', async (data)=>{
-      // console.log(data);
-      await publisher.publish("internal-server:checkbox:click", JSON.stringify(data));
-    })
-
-
-    socket.on('disconnect', async()=>{
-      count = await redis.decr(ACTIVE_USER_KEY);
-      await publisher.publish(ACTIVE_USER_KEY, count);
-    })
-
-  })
-
+  initRedisPubSub(io);
 
   //route for finding the total checked box
   app.get('/checked-box', (req, res)=>{
